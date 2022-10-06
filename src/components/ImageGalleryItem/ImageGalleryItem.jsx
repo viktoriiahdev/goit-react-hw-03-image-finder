430b94e68b50b981f0c8ed9ba6e52d1424340b55
Viktoriia Hrytsenko <dev.viktoriia.h@gmail.com>
2022-10-06 23:02:46 +0300

    Updates

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-import GalleryItem from './ImageGalleryItem.styled';
-
-const ImageGalleryItem = ({ src, alt, large, onClick }) => (
-  <GalleryItem className="ImageGalleryItem" onClick={() => onClick(large)}>
-    <img src={src} alt={alt} className="ImageGalleryItem-image" />
-  </GalleryItem>
-);
-
-export default ImageGalleryItem;
